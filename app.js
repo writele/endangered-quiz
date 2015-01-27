bbb0ce0fca75d912981ba5a1ded0bbd8ab064377
Ele Palmer <ecpalmer21@gmail.com>
2015-01-27 15:46:11 -0500

    Quiz Complete

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-
-    
 var correctAnswers = 0;
-var currentAnimal = 0;
-var previousAnimal = currentAnimal - 1;
+var currentAnimal = 1;
+var previousAnimal = 0;
     
-var quiz = {
-animal1: document.getElementById("animal1"),
-animal2: document.getElementById("animal2"),
-animal3: document.getElementById("animal3"),
-animal4: document.getElementById("animal4"),         
-animal5: document.getElementById("animal5")
-};
+var quiz = [
+document.getElementById("animal1"),
+document.getElementById("animal2"),
+document.getElementById("animal3"),
+document.getElementById("animal4"),         
+document.getElementById("animal5")
+];
 
+// SHOW NEXT ANIMAL function
 var nextAnimal = function() {
-    currentAnimal++;
+    if(currentAnimal == 5) {
+    finishQuiz();  
+    }
+    else {
     $(quiz[previousAnimal]).hide();
     $(quiz[currentAnimal]).show();
+    $(".choices").show();
+    $(".correctAnswer").hide();
+    $(".wrongAnswer").hide()
+    $(".next").hide();
+    currentAnimal++;
+    previousAnimal++;
+    }
 };
 
+// CHECK ANSWER function
 var checkAnswer = function() {
     if($(this).hasClass("correct")) {
     $(".choices").hide();
     $(".correctAnswer").show();
     $(".next").show();
+    correctAnswers++;
     }
     else {
     $(".choices").hide();
@@ -32,17 +43,34 @@ var checkAnswer = function() {
     }    
 };
 
-var newQuiz = function() {
+var showAnswer = function() {
+   $(".correctInfo").toggle(); 
+};
+
+var finishQuiz = function() {
     $("#animal5").hide();
+    $(".correctAnswer").hide();
+    $(".wrongAnswer").hide()
+    $(".finish").hide();
+    $("#finished").show();
+    $("#finished p span").html(correctAnswers);
+};
+
+var newQuiz = function() {
+    $(".next").hide();
+    $("#finished").hide();
     $("#intro").show();
     correctAnswers = 0;
-    currentAnimal = 0;
+    currentAnimal = 1;
+    previousAnimal = 0;
 };
+
 
 // START QUIZ    
 $(document).on("click", ".begin", function() {
     $("#intro").hide();
     $("#animal1").show();
+    $(".choices").show();
     $(".correctAnswer").hide();
     $(".wrongAnswer").hide();
     $(".next").hide();
@@ -51,8 +79,14 @@ $(document).on("click", ".begin", function() {
 // SELECT ANSWER
 $(document).on("click", "li", checkAnswer);
 
+// SHOW ANSWER
+$(document).on("click", ".showanswer", showAnswer);
+
 // CONTINUE
 $(document).on("click", ".next", nextAnimal);
+
+// FINISH
+$(document).on("click", ".finish", finishQuiz)
 
 // RESTART
 $(document).on("click", "#restart", newQuiz);
